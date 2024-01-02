733663888c5a6311fd147c2a8cfd88deb7dc84bb
EnochRibinChandrasekaran <155085038+EnochRibinChandrasekaran@users.noreply.github.com>
2024-01-02 10:41:22 +0000

    changed running time

--- DataStructures_JAVA/src/interview/questions/Anagram.java
+++ DataStructures_JAVA/src/interview/questions/Anagram.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 //Your task is to construct an algorithm to check whether two words (or phrases) are anagrams or not!
 //"An anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
 // typically using all the original letters exactly once"
-//For example: restful and fluster are anagrams. O(N)
+//For example: restful and fluster are anagrams.
+// Running time : O(NlogN)
 public class Anagram {
     public static void main(String[] args) {
         String wordOne = "restful";
