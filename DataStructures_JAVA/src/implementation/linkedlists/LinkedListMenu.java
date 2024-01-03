62b81d4e399d545e13bfebd5da4fa1e8118f9431
EnochRibinChandrasekaran <155085038+EnochRibinChandrasekaran@users.noreply.github.com>
2024-01-03 08:43:24 +0000

    added custom class with linked list

--- DataStructures_JAVA/src/implementation/linkedlists/LinkedListMenu.java
+++ DataStructures_JAVA/src/implementation/linkedlists/LinkedListMenu.java
@@ -3,30 +3,49 @@ package implementation.linkedlists;
 public class LinkedListMenu {
 
     public static void main(String[] args) {
-        List<Integer> linkedList = new LinkedLists<>();
-        linkedList.insert(1);
-        linkedList.insert(2);
-        linkedList.insert(3);
-        linkedList.insert(4);
+//        List<Integer> linkedList = new LinkedLists<>();
+//        linkedList.insert(1);
+//        linkedList.insert(2);
+//        linkedList.insert(3);
+//        linkedList.insert(4);
+//
+//        linkedList.traverse();
+//
+//        System.out.println("Size : " + linkedList.size());
+//
+//        linkedList.remove(1);
+//        linkedList.traverse();
+//
+//        System.out.println("Nodes :");
+//        linkedList.insert(12);
+//        linkedList.insert(11);
+//        linkedList.insert(110);
+//        linkedList.insert(1111);
+//
+//        linkedList.traverse();
+//        System.out.println("middle node :" + linkedList.getMiddleNode());
+//        linkedList.reverse();
+//        linkedList.traverse();
 
-        linkedList.traverse();
+        List<Person> personList = new LinkedLists<>();
 
-        System.out.println("Size : " + linkedList.size());
+        Person ribin = new Person("Ribin", 22);
+        personList.insert(new Person("Enoch", 25));
+        personList.insert(ribin);
+        personList.insert(new Person("Raj", 27));
+        personList.insert(new Person("Gopi", 28));
+        personList.insert(new Person("Jeff", 21));
+        personList.insert(new Person("James", 29));
 
-        linkedList.remove(1);
-        linkedList.traverse();
+        personList.traverse();
 
-        System.out.println("Nodes :");
-        linkedList.insert(12);
-        linkedList.insert(11);
-        linkedList.insert(110);
-        linkedList.insert(1111);
+        System.out.println("Middle element : " + personList.getMiddleNode());
+        personList.remove(ribin);
+        personList.traverse();
 
-        linkedList.traverse();
-        System.out.println("middle node :" + linkedList.getMiddleNode());
-        linkedList.reverse();
-        linkedList.traverse();
-
+        System.out.println("Reverse order the list :");
+        personList.reverse();
+        personList.traverse();
 
     }
 }
