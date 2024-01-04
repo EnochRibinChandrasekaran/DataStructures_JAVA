8baa32ec1f35713843c9cf4b6752ab231311c5af
EnochRibinChandrasekaran <155085038+EnochRibinChandrasekaran@users.noreply.github.com>
2024-01-04 06:26:49 +0000

    stack implementation in linked list

---
+++ DataStructures_JAVA/src/implementation/stacks/linkedlist/Stacks.java
@@ -0,0 +1,42 @@
+package implementation.stacks.linkedlist;
+
+public class Stacks<T> {
+    int count;
+    Node<T> head;
+
+    public int size() {
+        return count;
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    public void push(T data) {
+        if (head == null)
+            head = new Node<>(data);
+        else {
+            Node<T> oldHead = head;
+            head = new Node<>(data);
+            head.setNextNode(oldHead);
+        }
+        count++;
+    }
+
+    public T pop() {
+        if (head == null) return null;
+        else {
+            count--;
+            T oldHeadData = head.getData();
+            head = head.getNextNode();
+            return oldHeadData;
+        }
+
+    }
+
+    public T peek() {
+        if (head == null) return null;
+        else return head.getData();
+    }
+
+}
